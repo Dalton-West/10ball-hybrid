08eeed7025689d45ebcd5b19d6fae1c0225ff065
Damnuel Coleman West <daltonwest2@gmail.com>
2017-11-19 19:44:31 -0800

    Sync Desktop to Laptop
    Adding the signup change

--- hybid-bootstrap-vs/src/js/app_config.js
+++ hybid-bootstrap-vs/src/js/app_config.js
@@ -19,14 +19,46 @@
     messagingSenderId: "437989763197"
   };
   firebase.initializeApp(config);
-  //Firebase Auth
-  var auth = firebase.auth();
+
+//Firebase Componets
+const auth = firebase.auth();
+const db = firebase.firestore();
+const CURRENTUSER = firebase.User;
+
 //login logout buttons
 var logInBtn = document.getElementById("logIn");
 var logOutBtn = document.getElementById("logOut");
-var regContent = document.getElementById("registerContent");
 //Realtime authentication listener
-firebase.auth().onAuthStateChanged(function(user){
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // [START_EXCLUDE]
+	  //console.log("A user is signed in See Below");
+	  console.log(user);
+    logOutBtn.classList.remove("d-none");
+    logInBtn.classList.add("d-none");
+    // [END_EXCLUDE]
+  } else {
+    // User is signed out.
+    // [START_EXCLUDE]
+	  console.log("A user is not signed in!");
+    logOutBtn.classList.add("d-none");
+    logInBtn.classList.remove("d-none");
+    // [END_EXCLUDE]
+  }
+  // [START_EXCLUDE]
+  document.getElementById('quickstart-sign-in').disabled = false;
+  // [END_EXCLUDE]
+});
+/* firebase.auth().onAuthStateChanged(function(user){
 	if (user) {
 	  // User is signed in.
 	  //console.log("A user is signed in See Below");
@@ -41,7 +73,7 @@ firebase.auth().onAuthStateChanged(function(user){
     logInBtn.classList.remove("d-none");
 	}
   });
-
+*/
   //Sign Out
   logOutBtn.addEventListener("click", logOut);
 //Sign Out Users
